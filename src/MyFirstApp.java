57a353c5c7474e15cde6df09863e068ba2b04de8
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-09-29 14:37:31 +0700

    Initial commit

---
+++ src/MyFirstApp.java
@@ -0,0 +1,9 @@
+/**
+ * Created by anonymous.vn1985@gmail.com
+ */
+public class MyFirstApp {
+    public static void main(String[] args) {
+        System.out.println("I Rule");
+        System.out.println("Hello World");
+    }
+}
